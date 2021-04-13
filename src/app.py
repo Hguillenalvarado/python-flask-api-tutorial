1566f4e55c6c1219c2d8ba40adaca4a5ea143583
Hguillenalvarado <heinerguillen@gmail.com>
2021-04-13 03:25:41 +0000

    hasta el 7.2

--- src/app.py
+++ src/app.py
@@ -1,10 +1,24 @@
-from flask import Flask
-app = Flask(__name__)
+import flask
+import json
+from flask import request
+app = flask.Flask(__name__)
+todos = [
+    { "label": "My first task", "done": False },
+    { "label": "My second task", "done": False }
+] 
+   
 
-@app.route('/blabla', methods=['GET'])
+@app.route('/todos', methods=['GET'])
 def hello_world():
-    return '<h1>Hello!</h1>'
+    json_text=flask.jsonify(todos)
+    return json_text
 
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body=request.data
+    return "request"
+
+json.loads = json.loads(original_string)
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=3245, debug=True)
+    app.run(host='0.0.0.0', port=3245, debug=True)
